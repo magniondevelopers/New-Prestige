87a10dfa5bb2cbf8ef1de36e49f644e19db8e265
magniondevelopers <developers@magnion.co.in>
2026-08-25 23:04:01 +0530

    firebase integrated

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXyeG8d2Xqa4l6lw2oxRsMnovzWRPvcVc",
+  authDomain: "chatapp-f4237.firebaseapp.com",
+  databaseURL: "https://chatapp-f4237-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chatapp-f4237",
+  storageBucket: "chatapp-f4237.firebasestorage.app",
+  messagingSenderId: "830433078140",
+  appId: "1:830433078140:web:85182fb345151dc91ed2f9",
+  measurementId: "G-G1MR37FM94"
+};
